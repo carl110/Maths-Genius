102d482b7efbd13281755eb78a2f871cbf38d408
Carl Wainwright <carljame7@gmail.com>
2019-08-09 19:54:37 +0100

    Initial Commit

---
+++ Maths Genius/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Maths Genius
+//
+//  Created by Carl Wainwright on 09/08/2019.
+//  Copyright © 2019 Carl Wainwright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
