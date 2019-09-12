34a4020ab92f779e01cf9596117de689a5fa3274
Carl Wainwright <carljame7@gmail.com>
2019-09-12 12:33:44 +0100

    adding extensions for number to last on array

---
+++ Maths Genius/Extensions/Array+extensions.swift
@@ -0,0 +1,43 @@
+//
+//  Array+extensions.swift
+//  Maths Genius
+//
+//  Created by Carl Wainwright on 11/09/2019.
+//  Copyright © 2019 Carl Wainwright. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension Array {
+    
+    //provide last item of an array
+    var last: Element {
+        return self[self.endIndex - 1]
+    }
+    
+    var secondToLast: Element {
+        return self[self.endIndex - 2]
+    }
+    
+    var thirdToLast: Element {
+        return self[self.endIndex - 3]
+    }
+    
+    var fourthToLast: Element {
+        return self[self.endIndex - 4]
+    }
+    
+    var fithToLast: Element {
+        return self[self.endIndex - 5]
+    }
+    
+    var first: Element {
+        return self[self.startIndex]
+    }
+    
+    var secondToStart: Element {
+        return self [self.startIndex + 1]
+    }
+    
+}
