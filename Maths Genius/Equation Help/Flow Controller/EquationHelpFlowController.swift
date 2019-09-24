9b938a277c03e5a03815d15001a36f4607bce8cf
Carl Wainwright <carljame7@gmail.com>
2019-09-24 11:07:32 +0100

    adding uncommitted files

---
+++ Maths Genius/Equation Help/Flow Controller/EquationHelpFlowController.swift
@@ -0,0 +1,23 @@
+//
+//  EquationHelpFlowController.swift
+//  Maths Genius
+//
+//  Created by Carl Wainwright on 20/09/2019.
+//  Copyright © 2019 Carl Wainwright. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EquationHelpFlowController {
+    let navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    //Factory of view to show
+    func showMain() {
+        MainFactory.PushIn(navigationController: navigationController)
+    }
+}
